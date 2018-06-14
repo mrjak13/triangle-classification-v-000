6121492349ab83826ce6c05453f79921d3c1f470
Justin Kenney <justinkenney13@gmail.com>
2018-06-14 20:21:25 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ class Triangle
     elsif side_2 == side_3 || side_1 == side_3 || side_1 == side_2
       :isosceles
     elsif side_1 != side_2 && side_2 != side_3
+      :scalene
     end
   end
 end
