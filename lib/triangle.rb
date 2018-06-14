901fdbc97cb1bba3a10bf58fbe01ce8e8864a371
Justin Kenney <justinkenney13@gmail.com>
2018-06-14 20:01:42 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,8 @@ class Triangle
   # write code here
   attr_accessor :side_1, :side_2, :side_3
   def initialize (side_1, side_2, side_3)
-
-
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 end
